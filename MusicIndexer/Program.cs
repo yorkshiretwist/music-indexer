7e81ac19a404504c30be0ae5d7cca5a608564940
Chris Taylor <chris@stillbreathing.co.uk>
2020-04-24 08:01:16 +0100

    Initial scaffolded app

---
+++ MusicIndexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
