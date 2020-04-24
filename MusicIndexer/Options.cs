b6818e8f202255a286db620c8df809bf76f4abd3
Chris Taylor <chris@stillbreathing.co.uk>
2020-04-24 08:40:01 +0100

    Added command line arguments and startup checks

---
+++ MusicIndexer/Options.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CommandLine;
+
+namespace MusicIndexer
+{
+    /// <summary>
+    /// Represents options that can be passed via command line to the app
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Gets or sets a value indicating if verbose output should be enabled
+        /// </summary>
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the path at which to start scanning files
+        /// </summary>
+        [Option('p', "path", Required = true, HelpText = "Set the path at which to start scanning files.")]
+        public string Path { get; set; }
+    }
+}
--- MusicIndexer/Program.cs
+++ MusicIndexer/Program.cs
@@ -1,12 +1,92 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLine;
 
 namespace MusicIndexer
 {
     class Program
     {
+        static Options options;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Starting ... attempting to parse arguments");
+
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(Start)
+                   .WithNotParsed(HandleParseError);
+
+            Console.WriteLine($"Press 'enter' key to exit.");
+            Console.ReadLine();
+        }
+
+        static void HandleParseError(IEnumerable<Error> errors)
+        {
+            Error("Failed to parse command line arguments");
+            foreach(var error in errors)
+            {
+                Error($"- {error}", false);
+            }
+        }
+
+        static void Error(string message, bool lineAfter = true)
+        {
+            var fg = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(message);
+            Console.ForegroundColor = fg;
+            if (lineAfter)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        static void Verbose(string message, bool lineAfter = true)
+        {
+            var fg = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(message);
+            Console.ForegroundColor = fg;
+            if (lineAfter)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        static void Write(string message, bool lineAfter = true)
+        {
+            var fg = Console.ForegroundColor;
+            Console.WriteLine(message);
+            Console.ForegroundColor = fg;
+            if (lineAfter)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        static void Start(Options opts)
+        {
+            options = opts;
+
+            if (options.Verbose)
+            {
+                Write($"Verbose output enabled.");
+            }
+
+            if (!CheckPath(options.Path))
+            {
+                Error($"The path '{options.Path}' was not found or is not accessible");
+                return;
+            } else if (options.Verbose)
+            {
+                Verbose($"The path '{options.Path}' was found and is accessible");
+            }
+        }
+
+        private static bool CheckPath(string path)
+        {
+            return Directory.Exists(path);
         }
     }
 }
